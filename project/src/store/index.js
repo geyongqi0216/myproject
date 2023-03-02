d3e4308e2bc106123b94278fb92701b54907b6ab
geyongqi <15645002951@163.com>
2023-03-02 13:52:57 +0800

    VUEX: Basic use

---
+++ project/src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue"
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+const store = new Vuex.Store({
+    // state,  // 提供唯一的公共数据源
+    // mutations,  // 用于变更State中的数据（不能进行异步操作）
+    // actions,    // 用于处理异步操作（只有通过触发Mutations的方式才能间接变更State数据，不能直接修改State中数据）
+    // getters,    // 用于对State中的数据进行加工处理形成新的数据（state中的数据发生改变，Getter中的数据也跟着发生改变） 
+    // modules   // 内部仍然可以继续存放一些子store,内部再有五种属性，模块化vuex
+ 
+})
+
+export default store
