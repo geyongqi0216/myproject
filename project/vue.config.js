8e9807ccb0290fc50e38245b19d53c2c7fc93eae
geyongqi0216 <126649139+geyongqi0216@users.noreply.github.com>
2023-08-21 16:13:01 +0800

    Update vue.config.js
    Add css style

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true // transpileDependencies: ["vuetify"],
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          "primary-color": "#0071c5",
+          "link-color": "#096dd9",
+          "border-radius-base": "3px",
+          "font-family": "intel-clear",
+          "font-size-base": "14px",
+          "text-color": "#000000",
+          "disabled-color": "rgba(0, 0, 0, 0.5)",
+          "table-expanded-row-bg": "#f8fcff",
+        },
+        javascriptEnabled: true,
+      },
+      sass: {},
+    },
 })
