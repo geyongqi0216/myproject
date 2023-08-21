83a70edc1ac2c6047291b8fbe31107f8ca09448e
geyongqi0216 <126649139+geyongqi0216@users.noreply.github.com>
2023-08-21 16:39:22 +0800

    Create index.js
    api index

---
+++ project/src/api/index.js
@@ -0,0 +1,13 @@
+const modulesFiles = require.context('./modules', true, /\.js$/)
+
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  // set './app.js' => 'app'
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value
+  return modules
+}, {})
+
+const api = modules
+
+export default api
